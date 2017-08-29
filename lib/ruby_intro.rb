dc282186c0d7fbd7f2dc464bcfaee1683afe2667
Ella Nicolson <e.c.nicolson@gmail.com>
2017-08-29 14:27:59 -0500

    Solved part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,7 +4,7 @@
 
 def sum arr
   total = 0
-  arr.each { |i| total += i}
+  arr.each { |i| total += i }
   total
 end
 
@@ -19,6 +19,7 @@ def max_2_sum arr
   total
 end
 
+# MY WORK, class notes commented below
 def sum_to_n? arr, n
   answer = false
   for i in 0...arr.length
@@ -32,18 +33,54 @@ def sum_to_n? arr, n
   answer
 end
 
+=begin
+NOTES FROM CLASS
+def sum_to_n? arr, n
+  arr2 = arr.clone
+  arr.delete_at(0)
+  for el1 in arr
+    for el2 in arr2
+      if el1 + el2 == n
+        return true
+      end
+      arr.delete_at(0)
+    end
+  end
+  return false
+end
+
+
+def sum_to_n? arr, n
+  if arr.combination(2).find{ |x, y| x + y == n }
+    true
+  else false
+  end
+end
+=end
+
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  greeting = "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if /\A[b-df-hj-np-tv-z]/i.match(s)
+    true
+  else
+    false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s[/[01]+/] == s
+    if s.length == 1
+      return s == "0"
+    else
+      return s[s.length-3, s.length] == "100"
+    end
+  end
+  return false
 end
 
 # Part 3
