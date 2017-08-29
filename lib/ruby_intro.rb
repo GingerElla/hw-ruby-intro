31672b289e1b3fe29a7ec91b4dc9e5a023fc7521
Ella Nicolson <e.c.nicolson@gmail.com>
2017-08-29 02:25:06 -0500

    Answered part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,33 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+  arr.each { |i| total += i}
+  total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  total = 0
+  if arr.length == 1
+    total = arr[0]
+  elsif arr.length > 1
+    nums = arr.max(2)
+    total = nums[0] + nums[1]
+  end
+  total
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  answer = false
+  for i in 0...arr.length
+    for j in i+1...arr.length
+      if arr[i] + arr[j] == n
+        answer = true
+        break
+      end
+    end
+  end
+  answer
 end
 
 # Part 2
